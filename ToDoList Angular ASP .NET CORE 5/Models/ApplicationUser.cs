e477cdc8e3566c31c9a89ada8280e4a132df00f9
MichalKobarynko <kobarynkomichal@gmail.com>
2023-02-17 20:03:57 +0100

    Dodaj pliki projektów.

---
+++ ToDoList Angular ASP .NET CORE 5/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList_Angular_ASP_.NET_CORE_5.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
